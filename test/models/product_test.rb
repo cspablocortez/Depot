f13f1633c77f549ef8127f14a29ab3ec610217a4
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-05-17 18:52:49 -0700

    Test Fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,30 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
+  def new_product(image_url)
+    Product.new(title: "My Book Title",
+      description: "yyy",
+      price: 1,
+      image_url: image_url)
+  end
+
+  test "image url" do 
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg https://example.com/image.png }
+
+    bad = %w{ fred.docx elias.ppt landers.pdf definitely_not_a_virus_lol.exe }
+
+    ok.each do |image_url| 
+      assert new_product(image_url).valid?, "#{image_url} must be valid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} must be invalid"
+    end
+
+  end
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
